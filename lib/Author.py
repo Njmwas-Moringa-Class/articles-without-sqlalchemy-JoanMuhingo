513b0ee502a60b036ac1c21b12691cf8b1c9055f
Joan <joanmuhingo@gmail.com>
2024-02-05 16:11:12 +0300

    progress git commit -mprogress

--- lib/Author.py
+++ lib/Author.py
@@ -1,2 +1,39 @@
 class Author:
-    pass
+    _all.authors =[]
+   def __init__(self, name):
+        self._name = name 
+        Author._all_authors.append(self)     
+        self._articles =[]      
+
+
+    def get_name(self):
+
+        return self._name
+    
+
+    def article(self):
+
+        return self._articles
+    
+
+    def magazines(self):
+        unique_magazines = set(articles.get_magazine()for article in self ._articles)
+        return list(unique_magazines)    
+
+    def write_article(self, magazine, title):
+        new_article = Article(self, magazine, title)   
+        self._articles.append(new_article)
+
+    def add_article(self, magazine, title):
+
+        new_article = Article(self, magazine, title)
+        self._articles.append(new_article)
+
+    def topic_areas(self):
+
+        return list(set(article.get_magazine().get_category() for article in self._articles))    
+
+
+    def get_all_authours(cls):
+
+        return cls._all_authors
--- lib/Magazine.py
+++ lib/Magazine.py
@@ -1,2 +1,58 @@
 class Magazine:
-    pass
+
+    all.magazines = []
+
+    def __init__(self, name, category):
+        self._name = name
+        self._category = category
+        self._articles = []
+        Magazine._all_magazines.append(self)
+
+
+    def get_name(self):    
+        
+        return self.name
+
+    def get_category(self):   
+
+        return self.category
+
+    
+    def contributors(self):
+        
+        return self._contributors
+
+    def add_contributors(self, author):
+
+        if author not in self.add_contributors:
+            self.add_contributors.append(author)    
+    
+    @classmethod
+    def find_by_name(cls, name):
+
+        for magazine in cls.get_all_magazines:
+            if magazine.get_name() == name:
+                return magazine
+        return None        
+
+    @classmethod
+    def article_titles(cls):
+
+        all_titles= []
+        for magazine in cls.get_all_magazines:
+            all_titles.extend(article.get_title() for article in magazine._articles)
+        return all_titles
+
+    def contributing_authors(self):
+
+        authours_count ={}   
+        for articles in self._articles:
+            author = article.get.author()     
+            authors_count[author] = authors_count.get(author, 0) + 1
+
+        return [author for author, count in author_count.items()if count > 2]    
+    
+    @classmethod
+    def get_all_magazines(cls):  
+
+        return cls.all_magazines
